fd96d6b8a41c8d223fd32f8e7b522e31af8aeb3e
YooWoo <12hongkildong@gmail.com>
2023-03-21 01:34:58 +0900

    230321 선호님의 도움으로 복원 완료, 타임리프 두번째 시간

--- rlandboot3/src/main/java/kr/co/rland/web/controller/MenuController.java
+++ rlandboot3/src/main/java/kr/co/rland/web/controller/MenuController.java
@@ -1,22 +1,38 @@
 package kr.co.rland.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import kr.co.rland.web.entity.Menu;
+import kr.co.rland.web.entity.MenuView;
+import kr.co.rland.web.service.MenuService;
 
 
 @Controller
- 
 @RequestMapping("/menu")
 public class MenuController {
-
+	@Autowired
+	private MenuService service;
+	
 	@GetMapping("list") 
 	public String list(Model model) {
 		
-		model.addAttribute("data", "hello");
+		List<MenuView> list = service.getViewList();
+		model.addAttribute("list", list);
 		
+//		service.getList(); //1, quert:"", category:null
+//		service.getList(1);
+//		service.getList(1, "");
+//		service.getList(1, 1 );
+//		service.getList(1, 1,"" );
+		
+		
+		//model.addAttribute("data", "hello");
 		//return "menu list";
 		//return "/WEB-INF/view/menu/list.jsp";
 		//return "/WEB-INF/view/inc/layout.jsp";
@@ -25,7 +41,12 @@ public class MenuController {
 	}
 
 	@GetMapping("detail") 
-	public String detail() {
-		return "menu detail";
+	public String detail(int id, Model model) {
+		
+		Menu menu = service.getById(id);
+		
+		model.addAttribute("menu", menu);
+		
+		return "menu/detail";
 	}
 }
